e5c344e518259c66415549b374c3db7504c4f6dd
Sycheva Anastasia <za_sycha@mail.ru>
2016-10-20 14:01:29 +0500

    Parsing temperature in array

--- parse.py
+++ parse.py
@@ -1,10 +1,20 @@
 import urllib.request
 import re
 
+def convertByteToString(array):
+    for i in range(0, len(array)):
+        array[i] = array[i].decode('utf-8')
+
 infoPage = urllib.request.urlopen("http://rp5.ru/3034/ru")
 codeRP5 = infoPage.read()
-regex = re.compile(b'<div class="t_0">([+-]{0,1}\d{0,2})</b></div>')
-expectedTempr = re.findall(regex, codeRP5)
-for i in range(0, len(expectedTempr)):
-    expectedTempr[i] = expectedTempr[i].decode('utf-8')
-print(expectedTempr)
+templateForFeelLike = re.compile(b'<div class="t_0">([+-]?\d+)</b></div>')
+templateForTempr = re.compile(b'<div class="t_0"><b>([+-]?).{0,28}(\d+)</b></div>')
+tempr = templateForTempr.findall(codeRP5)
+feelLikeTempr = templateForFeelLike.findall(codeRP5)
+for i in range(0, len(tempr)):
+    tempr[i] = list(tempr[i])
+    tempr[i] = b''.join(tempr[i])
+convertByteToString(tempr)
+convertByteToString(feelLikeTempr)
+print(tempr)
+print(feelLikeTempr)
