648d3e9e841fad00d4182592f1002d2cb2df12d5
Matvey <xaxalox2@gmail.com>
2016-11-09 11:27:24 +0500

    Add some info

--- parse.py
+++ parse.py
@@ -1,3 +1,6 @@
+# вытащили температуру, ощущается как, давление и влажность
+# осталось местное время - сконвертировать, облачность/явления погоды, ветер
+# солнце/луна/фаза - надо или нет?
 import urllib.request
 import re
 
@@ -5,25 +8,45 @@ def convertByteToString(array):
     for i in range(0, len(array)):
         array[i] = array[i].decode('utf-8')
 
-infoPage = urllib.request.urlopen("http://rp5.ru/3034/ru") # get code site
-codeRP5 = infoPage.read() # read code site
+def convertToNormalView(array, separator):
+    for i in range(0, len(array)): 
+        array[i] = list(array[i])
+        array[i] = separator.join(array[i])
 
-templateForFeelLike = re.compile(b'<div class="t_0">([+-]?\d+)</b></div>') # regular for feels temperature
-templateForTempr = re.compile(b'<div class="t_0"><b>([+-]?).{0,28}(\d+)</b></div>') # regular for temperature
-templateForPressure = re.compile(b'<div class="p_0"><b>(\d+)</b></div>') # template for pressure
-templateForWet = re.compile(b'\)">([0-9]+)</td>') # regular for wet
-tempr = templateForTempr.findall(codeRP5) #find all temperature
-feelLikeTempr = templateForFeelLike.findall(codeRP5) # find all feel like temperature
-pressure = templateForPressure.findall(codeRP5) # find all pressure
-wet = templateForWet.findall(codeRP5) # find all wet
-for i in range(0, len(tempr)): # convert tempr to normal view
-    tempr[i] = list(tempr[i])
-    tempr[i] = b''.join(tempr[i])
+infoPage = urllib.request.urlopen("http://rp5.ru/3034/ru") # получаем код сайта rp5 погода в Екатеринбурге
+codeRP5 = infoPage.read() # записываем код в переменную
+
+templateForFeelLike = re.compile(b'<div class=\"t_0\">([+-]?\d+)</b></div>') # регулярное выражение для "Ощущается как, °C"
+templateForTempr = re.compile(b'<div class=\"t_0\"><b>([+-]?).{0,28}(\d+)</b></div>') # регулярное выражение для "Температура, °C"
+templateForPressure = re.compile(b'<div class=\"p_0\">[<b>]{0,3}(\d+)[</b>]{0,3}</div>') # регулярное выражение для "Давление, мм рт. ст."
+templateForWet = re.compile(b'\)\">[<b>]{0,3}(\d+)</td>') # регулярное выражение для "Влажность, %"
+templateForCloudiness = re.compile(b'<div class=\"cc_0\">.+<b>(.+)</b><br/>\((.+)\)\'') # регулярное выражение для "Облачность, %"
+templateForPhenomenon = re.compile(b'<div class=\"pr_0\".+\'(.+)\' ,') # регулярное выражение для "Явления погоды"
+templateForPhenomenon1 = re.compile(b' class=\" litegrey .{1,2}.+\%\" .+\'(.+)\' ,') # регулярное выражение для "Явления погоды1"
+
+tempr = templateForTempr.findall(codeRP5) # получаем значения температуры
+feelLikeTempr = templateForFeelLike.findall(codeRP5) # получаем значение "ощущается как"
+pressure = templateForPressure.findall(codeRP5) # получаем значения давления
+wet = templateForWet.findall(codeRP5) # получаем значения влажности
+cloudiness = templateForCloudiness.findall(codeRP5) # получаем значения облачности
+phenomenon = templateForPhenomenon.findall(codeRP5) # получаем значения явлений погоды
+phenomenon1 = templateForPhenomenon1.findall(codeRP5) # получаем значения явлений погоды
+
+convertToNormalView(tempr, b'')
+convertToNormalView(cloudiness, b' ')
+
 convertByteToString(tempr)
 convertByteToString(feelLikeTempr)
 convertByteToString(pressure)
 convertByteToString(wet)
+convertByteToString(cloudiness)
+convertByteToString(phenomenon)
+convertByteToString(phenomenon1)
+
 print(tempr)
 print(feelLikeTempr)
 print(pressure)
 print(wet)
+print(cloudiness)
+print(phenomenon)
+print(phenomenon1)
